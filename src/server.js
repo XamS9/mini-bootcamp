be0a57d4a03d40b78da1cc55e437e933e6346174
XamS9 <67987570+XamS9@users.noreply.github.com>
2023-01-16 13:18:30 -0400

    First connection to express

--- src/server.js
+++ src/server.js
@@ -1,8 +1,13 @@
 const express = require('express')
 const app = express()
+const PORT = process.env.PORT || 3001;
+const cors = require("cors");
+app.use(cors());
 
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
+app.get("/api", (req, res) => {
+  res.json({ message: "Hi from server!" });
+});
 
-app.listen(3001)
+app.listen(PORT, () => {
+  console.log(`Server listenning on port: ${PORT}`);
+});
